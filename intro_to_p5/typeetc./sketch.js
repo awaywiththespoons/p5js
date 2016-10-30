d176bfc9ef048f12279cee86793069db8b3f9842
Becca Rose <hello@beccarose.co.uk>
2016-10-30 12:15:06 +0000

    fun abc timez

--- intro_to_p5/typeetc./sketch.js
+++ intro_to_p5/typeetc./sketch.js
@@ -1,39 +1,59 @@
  var textA = 12;
- var textB = 12;
+ var textB = 800;
  var textC = 12;
+
+ var angleA = .25;
+ var widthB = 0;
+ var new_widthB;
 
  function setup() {
    createCanvas(600, 600);
+   textAlign(CENTER);
+
+   new_widthB = width;
+
  }
 
  function draw() {
    background(200, 20, 103);
-   textSize(12);
-   text("press ~ a ~ b or ~ c for ~ joy", 20, 20)
+   textSize(14);
+   text("press ~ a ~ b or ~ c for ~ joy", width / 2, 20);
 
    if (keyIsPressed) {
-     // A
-     if (keyCode == 097 && textA < 573) {
-       textSize(textA);
-       text("a", width / 2, height / 2);
-       textA++;
-       console.log(textA);
-     } else if (keyCode == 097 && textA >= 573) {
+     // A grows up
+     if (keyCode == 097) {
        for (var i = 0; i < 10; i++) {
-         textSize(12);
-         text("a", width * .75 + i, height / 4 + i)
+         textSize(573);
+         rotate(angleA);
+         text("a", width / 2, height / 1.5);
+         angleA = random(0, 360);
        }
-     } //add to this by making some animated letters when you get really big
+     }
 
-     // B
+     // B B B B B B B B 
      else if (keyCode == 098) {
-       textSize(textB);
-       text("b", width / 2, height / 2);
-       textB++;
-       console.log(textB);
-     } else if (keyCode == 099) {
+       if (widthB < width) {
+         textSize(textB);
+         text("b", widthB, height);
+         widthB += 5;
+         console.log("old w B = " + widthB);
+       }
 
-       // C
+       if (widthB >= width) {
+         textSize(textB);
+         text("b", new_widthB, height);
+         new_widthB -= 5;
+         console.log("new wb = " + new_widthB);
+         //but if new width possition is 0 go to first loop
+         if (new_widthB === 0) {
+           widthB = 0;
+         }
+       }
+     }
+     // C
+     else if (keyCode == 099) {
+
+
        textSize(textC);
        text("c", width / 2, height / 2);
        textC++;
