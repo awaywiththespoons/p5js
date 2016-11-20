b9fe5f25311ceab6c3bb49440857a6015f20de2c
Becca Rose <hello@beccarose.co.uk>
2016-11-20 22:19:04 +0000

    getting data on load

--- intro_to_p5/drawgrammingV2/sketch.js
+++ intro_to_p5/drawgrammingV2/sketch.js
@@ -1,24 +1,21 @@
-//inspired automated sketches of by james paterson http://presstube.com/blog/category/james-paterson/ i wanted to experiment with 
-//some of my drawings and see what occured
-//becca rose 2016
-
-// this kind of evelved into a random book cover generator
+// inspired automated sketches of by james paterson http://presstube.com/blog/category/james-paterson/ i wanted to experiment with 
+// some of my drawings that are on my laptop already...and see what occured
+// becca rose 2016
+// this kind of evelved into a random image generator with titles of the images comign from book titles from open libary data
 
 // next 
 // : the bok name does not work until 2nd interation on "more books"
 // : I am not sure how to start with a book name
-// : add more images to data set
 
 var img = [];
 
 var imgW;
-var aspect;
 var imgH;
 var randISBN1;
 var randISBN2;
+
 var imageSnapButton
 var book;
-var bookStart;
 var textW;
 
 function preload() {
@@ -27,7 +24,7 @@ function preload() {
     img[i] = loadImage(i + ".png");
     println(i);
   }
-}
+} // end of preload
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -36,45 +33,31 @@ function setup() {
   imageMode(CENTER);
   noStroke();
 
-
+  newimage();
   // create button
   imageSnapButton = createButton('- M O R E -');
   imageSnapButton.position(40, 20);
-  imageSnapButton.mousePressed(imageSnap);
+  imageSnapButton.mousePressed(newImage);
 
-}
+} // end of setup
 
-function draw() {
-  imageSnap();
-}
-
-//find the JSON data page
-function newBook() {
+function jason() {
+  // get a title in the setup
   randISBN1 = ~~random(256);
   randISBN2 = ~~random(256);
   loadJSON('http://openlibrary.org/api/get?key=/b/OL1' + randISBN1 + randISBN2 + 'M', gotData);
-
 }
 
-// pinpoint the data I want to make the image titles
+//pinpoint the data I want to make the image titles
 function gotData(data) {
   book = data.result.title;
+  // get the size of each book string
   textW = textWidth(book);
-  println("book = " + book);
-  println("textW =  " + textW);
-  fill(20, 255, 70, 125)
-  rect(38, 80 - 32, textW + 2, 39);
-  fill(0);
-  textSize(35);
-  text(book, 40, 80);
-  println("textW end of gotData =  " + textW);
+} // end of got data
 
-}
-
-//function to make those imaages yeah.
-function imageSnap() {
+function newImage() {
   background(255);
-  //image randoms
+  // make some random numbers
   var rand1 = ~~random(35);
   var rand2 = ~~random(35);
   var rand3 = ~~random(35);
@@ -83,5 +66,14 @@ function imageSnap() {
   image(img[rand3], (width / 2) - 200, (height / 2) - 200, imgW, imgH);
   image(img[rand2], width / 2, (height / 2) - 100, imgW, imgH);
   image(img[rand1], (width / 2) + 200, (height / 2) + 30, imgW, imgH);
-  newBook();
+
+  //add the text
+  jason();
+  if (book) {
+    fill(20, 255, 70, 125)
+    rect(38, 80 - 32, textW + 2, 39);
+    fill(0);
+    textSize(35);
+    text(book, 40, 80);
+  }
 }
