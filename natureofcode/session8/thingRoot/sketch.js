41be1ae2b6f5aa7d1812cb69c6b16833a6ca9065
Becca Rose <hello@beccarose.co.uk>
2017-01-18 22:41:17 +0000

    more snap

--- natureofcode/session8/thingRoot/sketch.js
+++ natureofcode/session8/thingRoot/sketch.js
@@ -8,6 +8,11 @@ var angle;
 var imgW;
 var imgH;
 var img;
+var rootButton;
+
+var rootleng = 100;
+var rootlengLeft = 130;
+var rootlenRight = 105;
 
 function preload() {
   img = loadImage("18.png");
@@ -15,8 +20,10 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(900, 700);
   stroke(0);
+  // branchButton = createButton('root me up');
+  //branchButton.position(40, 20);
 
   noLoop();
 
@@ -25,8 +32,6 @@ function setup() {
 
 function draw() {
   angle = random(PI / 16, PI / 2);
-  background(19, 200, 60);
-
 
   //nf(num, left unit, right unit)
   println("angle = " + nf(angle, 1, 3));
@@ -35,31 +40,96 @@ function draw() {
   //start from the bottom, center of the canvas (using translate as not to complicate things by using X, Y coordinates
   //this will be a global tranlates to all function as it is in the draw and called before the function
 
+  background(19, 200, 60);
+  translate((width / 2)+100, height / 4);
 
-  translate(width / 2, height / 4);
-
-  image(img, 0, 0, imgW, imgH);
-  rotate(PI);
-
-
+  strokeWeight(3);
   //call function
-  branch(120);
+  push();
+  roots(rootleng);
+  translate(-rootleng, -rootleng);
+  rootsLeft(rootlengLeft);
+  translate(-rootlengLeft, -rootlengLeft);
+  rootsRight(rootlenRight);
+  // branchButton.mousePressed(branch(100));
+  pop();
 
-
+  noStroke();
+  fill(19, 200, 60);
+  rect(-250, -270, 400, 400);
+  tint(125);
+  image(img, 0, 0, imgW, imgH);
 }
-
 
 //this will draw an individual branch
 //generation changes the thickess of line each time
-function branch(length) {
+function roots(length) {
 
   //draw a sinle line
-  stroke()
-  line(0, 0, 0, -length);
+
+  line(0, 0, 0, length);
   // move line to end of the single line
-  translate(0, -length);
+  translate(0, length);
   //shrink the line
-  length *= random(0.6, 0.7);
+  length *= random(0.4, 0.9);
+
+  //now for getting fractaul
+  if (length > 6) {
+    //get a random number to make n branches 
+    //floor function makes intergers only
+
+    var n = floor(random(1, 4));
+    for (var i = 0; i < n; i++) {
+      //log where you push the whole thing
+      push();
+      rotate(random(-PI / 3, PI / 3));
+      roots(length)
+        //pop out of the log
+      pop();
+    }
+
+  }
+}
+
+function rootsLeft(lengthLeft) {
+
+  //draw a sinle line
+  //RED
+  //stroke(200, 40, 20);
+  line(0, 0, 0, lengthLeft);
+  // move line to end of the single line
+  translate(0, lengthLeft);
+  //shrink the line
+  lengthLeft *= random(0.4, 0.9);
+
+  //now for getting fractaul
+  if (lengthLeft > 4) {
+
+    //get a random number to make n branches 
+    //floor function makes intergers only
+
+    var n = floor(random(1, 4));
+    for (var i = 0; i < n; i++) {
+      //log where you push the whole thing
+      push();
+      rotate(random(-PI / 3, PI / 3));
+      roots(lengthLeft)
+        //pop out of the log
+      pop();
+    }
+
+  }
+}
+
+function rootsRight(length) {
+  //draw a sinle line
+  //BLUE
+  //stroke(49, 69, 200);
+  line(0, 0, 0, length);
+  // move line to end of the single line
+  translate(0, length);
+  //shrink the line
+  length *= random(0.4, 0.9);
 
   //now for getting fractaul
   if (length > 6) {
@@ -67,15 +137,18 @@ function branch(length) {
     //get a random number to make n branches 
     //floor function makes intergers only
 
-    var n = floor(random(1, 5));
+    var n = floor(random(1, 4));
     for (var i = 0; i < n; i++) {
       //log where you push the whole thing
       push();
       rotate(random(-PI / 3, PI / 3));
-      branch(length)
+      roots(length)
         //pop out of the log
       pop();
     }
 
   }
 }
+
+
+
